580c22daacfc2f8639096e3be9e547c8f377bfb8
radian628 <70352880+radian628@users.noreply.github.com>
2023-08-21 12:37:51 -0700

    added staticmath + fixed embeds

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,7 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-const createDesmosEmbedCode = (
-  state,
-  settings
-) => `<div class="desmos-container"></div><script>{ 
-  let calcContainer = document.currentScript.previousSibling;
-  let observer = new IntersectionObserver(() => {
-    let graphstate = ${state}; 
-    let Calc = Desmos.GraphingCalculator(calcContainer, ${settings || "{}"});
-    Calc.setState(graphstate);
-  });
-  observer.observe(calcContainer);
-}</script>`;
+const createDesmosEmbedCode = (state, settings) =>
+  `<div class="desmos-container invisible"><div>${state}</div><div>${settings}</div></div>`;
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -26,6 +16,9 @@ module.exports = function (eleventyConfig) {
     const cfg = tm.buildConfig({});
     const raw = tm.textToRaw(cfg, state);
     return createDesmosEmbedCode(JSON.stringify(raw[1]), settings);
+  });
+  eleventyConfig.addShortcode("staticmath", (state) => {
+    return `<span class="static-math-container dcg-calculator-api-container">${state}</span>`;
   });
 
   function handlePrefixedNav(nav) {
--- src/list-manipulation/update-single-list-element.md
+++ src/list-manipulation/update-single-list-element.md
@@ -11,6 +11,12 @@ tags:
 
 Here is a function that updates a single element in a list:
 
+{% staticmath 'l_{ist}' %} - The list whose single element is changing.
+
+{% staticmath 'i_{ndex}' %} - The one-based index of the element you're changing (e.g. pass in {% staticmath '3' %} to change the third element).
+
+{% staticmath 'n_{ewValue}' %} - The new value you're replacing the element with.
+
 {% textmode
-'update(list, i_ndex, newValue) = { [1 ... length(list)] = i_ndex: newValue, list }', '{ graphpaper: false }'
+'update(list, i_ndex, newValue) = { [1 ... length(list)] = i_ndex: newValue, list }', '{ "graphpaper": false }'
 %}

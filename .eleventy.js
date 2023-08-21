788bb0c981e7b3ca17cdf763b115031818d5226d
radian628 <70352880+radian628@users.noreply.github.com>
2023-08-21 00:55:33 -0700

    text mode templates support

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,29 @@
+const createDesmosEmbedCode = (
+  state,
+  settings
+) => `<div class="desmos-container"></div><script>{ 
+  let calcContainer = document.currentScript.previousSibling;
+  let observer = new IntersectionObserver(() => {
+    let graphstate = ${state}; 
+    let Calc = Desmos.GraphingCalculator(calcContainer, ${settings || "{}"});
+    Calc.setState(graphstate);
+  });
+  observer.observe(calcContainer);
+}</script>`;
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/**");
   eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addShortcode(
-    "graphstate",
-    (state, settings) => `<div class="desmos-container"></div><script>{ 
-    let graphstate = ${state}; 
-    let Calc = Desmos.GraphingCalculator(document.currentScript.previousSibling, ${settings});
-    Calc.setState(graphstate);
-  }</script>`
+  eleventyConfig.addShortcode("graphstate", (state, settings) =>
+    createDesmosEmbedCode(state, settings)
   );
+  eleventyConfig.addShortcode("textmode", async (state, settings) => {
+    const tm = await import("@desmodder/text-mode-core");
+
+    const cfg = tm.buildConfig({});
+    const raw = tm.textToRaw(cfg, state);
+    return createDesmosEmbedCode(JSON.stringify(raw[1]), settings);
+  });
   return {
     dir: {
       input: "src",
--- src/index.md
+++ src/index.md
@@ -5,3 +5,5 @@ title: Unofficial Desmos Wiki
 Welcome to the Unofficial Desmos Wiki, the collaborative site for sharing knowledge about the [Desmos Graphing Calculator](https://www.desmos.com/calculator) and other Desmos tools!
 
 Want to add content? [Submit a PR!](https://github.com/DesModder/DesModder-website)
+
+{% textmode 'y=x' %}
